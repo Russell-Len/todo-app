18f2716a1951f638ca4b5eeadb2f1ee321b7e725
Russell Len <russell.len@bertelsmann.de>
2023-03-14 16:35:25 +0800

    Created initial task list design Also made toolbar sticky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 import { TaskAddDialogComponent } from './task-add-dialog/task-add-dialog.component';
 import { TaskEditDialogComponent } from './task-edit-dialog/task-edit-dialog.component';
 import { TaskDeleteDialogComponent } from './task-delete-dialog/task-delete-dialog.component';
 import { TaskListComponent } from './task-list/task-list.component';
+
+//Angular Material Components
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon'
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -24,6 +31,11 @@ import { TaskListComponent } from './task-list/task-list.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
