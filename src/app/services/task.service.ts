b43f9eea0de40c3c10ddf0fb5defae58cf0939a9
Russell Len <russell.len@bertelsmann.de>
2023-03-22 17:16:44 +0800

    Added jwt to request headers

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { ITask } from '../model/ITask';
@@ -11,22 +11,24 @@ export class TaskService {
 
     private tasksApiUrl = `${environment.apiBaseURL}/tasks`;
 
+    private headers = { headers: new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`) }
+
     constructor(private http: HttpClient) { }
 
     getTasks(): Observable<ITask[]> {
-        return this.http.get<ITask[]>(this.tasksApiUrl);
+        return this.http.get<ITask[]>(this.tasksApiUrl, this.headers);
     }
 
     addTask(newTask: ITask): Observable<any> {
-        return this.http.post(this.tasksApiUrl, newTask);
+        return this.http.post(this.tasksApiUrl, newTask, this.headers);
     }
 
     editTask(task: ITask): Observable<any> {
-        return this.http.put(this.tasksApiUrl, task);
+        return this.http.put(this.tasksApiUrl, task, this.headers);
     }
 
     deleteTask(id: number): Observable<any> {
-        return this.http.delete(`${this.tasksApiUrl}/${id}`);
+        return this.http.delete(`${this.tasksApiUrl}/${id}`, this.headers);
     }
 
     getDueDateTime(dueDate: Date, dueTime: string): Date {
